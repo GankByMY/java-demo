57efa3515fc14c5e234b085d3f280148fccf9534
xielei <xielei@swifthealth.cn>
2020-06-08 15:24:30 +0800

    delete stringtest

--- src/main/java/org/xielei/string/StringTest.java
+++ src/main/java/org/xielei/string/StringTest.java
@@ -1,19 +1,9 @@
 package org.xielei.string;
 
+/**
+ * @author xielei
+ */
 public class StringTest {
 
-    public static void main(String[] args) {
-        String a = "abc";
-        String b = "abc";
-        String c = new String("abc");
-        String d = new String("abc");
 
-        System.out.println(a == b);
-        System.out.println(a == c);
-        System.out.println(c == d);
-        System.out.println(a.hashCode());
-        System.out.println(c.hashCode());
-        System.out.println(d.hashCode());
-
-    }
 }
